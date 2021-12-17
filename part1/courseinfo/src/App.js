8c3cf89e5dce179846a13115c390d5b1d3a7aed8
Martin Campuzano <martin.campuzano.flores@gmail.com>
2021-12-17 19:14:19 -0400

    Finished 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,25 +8,22 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  return (
+const Part = (props) => {
+  return(
     <div>
       <p>
-        {part1} {exercises1}
+        {props.part} {props.exercise}
       </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]} exercise={props.exercises[0]}/>
+      <Part part={props.parts[1]} exercise={props.exercises[1]}/>
+      <Part part={props.parts[2]} exercise={props.exercises[2]}/>
     </div>
   )
 }
@@ -51,7 +48,7 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content />
+      <Content parts={[part1,part2,part3]} exercises={[exercises1,exercises2,exercises3]}/>
       <Total total={exercises1+exercises2+exercises3}/>
     </div>
   )
