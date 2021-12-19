4d02d5e038ca7346b470255ad9dd5675acfe7b2d
Martin Campuzano <martin.campuzano.flores@gmail.com>
2021-12-19 14:28:30 -0400

    Finished 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,55 @@
 import React, { useState } from "react";
 
+const Filter = ({ filter, handleFilter }) => {
+    return (
+        <div>
+            <div>
+                filter shown with:{" "}
+                <input value={filter} onChange={handleFilter} />
+            </div>
+        </div>
+    );
+};
+
+const Person = ({ person }) => {
+    return (
+        <>
+            <div>
+                {person.name} {person.number}
+            </div>
+        </>
+    );
+};
+
+const Persons = ({ persons }) => {
+    return (
+        <>
+            {persons.map((person) => (
+                <Person person={person} key={person.id}/>
+            ))}
+        </>
+    );
+};
+
+const PersonForm = (props) => {
+  return(
+    <>
+      <form onSubmit={props.addNewEntry}>
+                <div>
+                    name: <input value={props.newName} onChange={props.handleNewName} />
+                </div>
+                <div>
+                    number:{" "}
+                    <input value={props.newNumber} onChange={props.handleNewNumber} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+    </>
+  )
+}
+
 const App = () => {
     const [persons, setPersons] = useState([
         { name: "Arto Hellas", number: "040-123456", id: 1 },
@@ -23,7 +73,12 @@ const App = () => {
         setFilter(event.target.value);
     };
 
-    const personsToShow = filter === "" ? persons : persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()))
+    const personsToShow =
+        filter === ""
+            ? persons
+            : persons.filter((person) =>
+                  person.name.toLowerCase().includes(filter.toLowerCase())
+              );
 
     const addNewEntry = (event) => {
         event.preventDefault();
@@ -46,31 +101,11 @@ const App = () => {
     return (
         <div>
             <h2>Phonebook</h2>
-            <div>
-                <div>
-                    filter shown with:{" "}
-                    <input value={filter} onChange={handleFilter} />
-                </div>
-            </div>
+            <Filter filter={filter} handleFilter={handleFilter} />
             <h1>add a new</h1>
-            <form onSubmit={addNewEntry}>
-                <div>
-                    name: <input value={newName} onChange={handleNewName} />
-                </div>
-                <div>
-                    number:{" "}
-                    <input value={newNumber} onChange={handleNewNumber} />
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
-            </form>
+            <PersonForm addNewEntry={addNewEntry} newName={newName} handleNewName={handleNewName} newNumber={newNumber} handleNewNumber={handleNewNumber}/>
             <h2>Numbers</h2>
-            {personsToShow.map((person) => (
-                <div key={person.id}>
-                    {person.name} {person.number}
-                </div>
-            ))}
+            <Persons persons={personsToShow} />
         </div>
     );
 };
