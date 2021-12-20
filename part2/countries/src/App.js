3b45348c7665cb3799671b6dff4d9bd9165adb56
Martin Campuzano <martin.campuzano.flores@gmail.com>
2021-12-19 23:17:42 -0400

    Finished 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Country = ({ name, setSearch, setFilter }) => {
-  const showHandler = () => {
-    setSearch(name)
-    setFilter([name])
-  }
+    const showHandler = () => {
+        setSearch(name);
+        setFilter([name]);
+    };
     return (
         <div>
             <br />
@@ -15,16 +15,38 @@ const Country = ({ name, setSearch, setFilter }) => {
 };
 
 const Detail = ({ country }) => {
-    return(
-      <>
-      <h1>{country.name.official}</h1>
-      <h2>languages</h2>
-      <ul>
-      {Object.values(country.languages).map((lang) => <li>{lang}</li>)}
-      </ul>
-      <img src={country.flags.png} alt="Flag"/>
-      </>
-    )
+    const [weather, setWeather] = useState([]);
+    const api_key = process.env.REACT_APP_API_KEY;
+    useEffect(() => {
+        axios
+            .get(
+                `https://api.openweathermap.org/data/2.5/onecall?lat=${country.latlng[0]}&lon=${country.latlng[1]}&appid=${api_key}&units=metric`
+            )
+            .then((response) => {
+                setWeather(response.data);
+            });
+    }, []);
+    return (
+        <>
+            <h1>{country.name.official}</h1>
+            Capital {country.capital}
+            <br />
+            Population {country.population}
+            <h2>languages</h2>
+            <ul>
+                {Object.values(country.languages).map((lang) => (
+                    <li key={lang}>{lang}</li>
+                ))}
+            </ul>
+            <img src={country.flags.png} alt="Flag" />
+            <h2>Weather in {country.capital}</h2>
+            Current: {weather.current.temp} °C
+            <br />
+            Pressure: {weather.current.pressure}
+            <br />
+            Wind Speed: {weather.current.wind_speed} mph
+        </>
+    );
 };
 
 const App = () => {
@@ -54,9 +76,20 @@ const App = () => {
             {filter.length > 10 ? (
                 <p>Too many countries, be more specific</p>
             ) : filter.length === 1 ? (
-                <Detail country={countries.find(country => country.name.common.toLowerCase().includes(filter))}/>
+                <Detail
+                    country={countries.find((country) =>
+                        country.name.common.toLowerCase().includes(filter)
+                    )}
+                />
             ) : (
-                filter.map((country) => <Country key={country} name={country} setSearch={setSearch} setFilter={setFilter}/>)
+                filter.map((country) => (
+                    <Country
+                        key={country}
+                        name={country}
+                        setSearch={setSearch}
+                        setFilter={setFilter}
+                    />
+                ))
             )}
         </div>
     );
