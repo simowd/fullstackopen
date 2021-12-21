d6a5ed717e5110a5e76a1e34dbd588285e0301a0
Martin Campuzano <martin.campuzano.flores@gmail.com>
2021-12-21 19:03:40 -0400

    Created bloglist project

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -149,6 +149,7 @@ const errorHandler = (error, request, response, next) => {
 // this has to be the last loaded middleware.
 app.use(errorHandler)
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
