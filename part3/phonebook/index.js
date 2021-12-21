1a6fded41d20bd2804889db5d33dd028524bdc7b
Martin Campuzano <martin.campuzano.flores@gmail.com>
2021-12-21 16:34:10 -0400

    Finished 3.19

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -88,7 +88,7 @@ app.delete("/api/persons/:id", (request, response, next) => {
         .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
     const person = request.body;
 
     if (!person.name || !person.number) {
@@ -97,30 +97,19 @@ app.post("/api/persons", (request, response) => {
         });
     }
 
-    Person.find({})
-        .then((result) => {
-            if (result.find((p) => p.name === person.name)) {
-                return response
-                    .status(409)
-                    .json({ error: "name must be unique" });
-            }
-        })
-        .catch((result) => {
-            response.status(500).send();
-        });
-
-    //Old code
-    /*const id = idGenerator(MAX_ID);
-    const newPerson = { id: id, ...person };
-    persons = persons.concat(newPerson);*/
     const newPerson = new Person({
         name: person.name,
         number: person.number,
     });
 
-    newPerson.save().then((savedPerson) => {
-        response.json(savedPerson);
-    });
+    newPerson
+        .save()
+        .then((savedPerson) => {
+            response.json(savedPerson);
+        })
+        .catch((error) => {
+            next(error);
+        });
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
@@ -156,6 +145,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === "CastError") {
         return response.status(400).send({ error: "malformatted id" });
+    } else if (error.name === "ValidationError") {
+        return response.status(400).json({ error: error.message });
     }
 
     next(error);
--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 require("dotenv").config();
 const uri = process.env.MONGODB_URI;
 
@@ -12,16 +13,17 @@ mongoose
     });
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {type:String, required: true, unique: true},
+    number: {type:String, required: true, unique: true},
 });
 
-personSchema.set('toJSON', {
+personSchema.plugin(uniqueValidator);
+personSchema.set("toJSON", {
     transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    },
+});
 
 module.exports = mongoose.model("Person", personSchema);
