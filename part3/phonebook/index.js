ed74411a0b562456aa58c2667a78e1eee3ea6a81
Martin Campuzano <martin.campuzano.flores@gmail.com>
2021-12-20 20:57:34 -0400

    Finished 3.11

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -9,6 +9,7 @@ app.use(morgan(":method :url :status :res[content-length] - :response-time ms :b
 
 app.use(express.json());
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     {
