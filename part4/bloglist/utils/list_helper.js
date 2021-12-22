d8b03dac17d58ebeef32fed82643f368a718dd90
Martin Campuzano <martin.campuzano.flores@gmail.com>
2021-12-21 20:39:56 -0400

    Finished 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -4,8 +4,25 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => blogs.reduce((total, blog) => total + blog.likes, 0)
 
+const favoriteBlog = (blogs) => {
+    const blog = blogs.reduce((current, blog) => {
+        if(current.likes){
+            return blog.likes > current.likes ? blog : current
+        }
+        else{
+            return blog
+        }
+    }, {})
+    const newObject = {
+        title: blog.title,
+        author: blog.author,
+        likes: blog.likes
+    }
+    return newObject
+}
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
