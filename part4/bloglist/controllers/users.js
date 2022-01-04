b97534127c3b795bf736cc669ad867263ce95a71
Martin Campuzano <martin.campuzano.flores@gmail.com>
2022-01-03 20:35:30 -0400

    Finished 4.15

---
+++ part4/bloglist/controllers/users.js
@@ -0,0 +1,26 @@
+const userRouter = require('express').Router()
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+const user = require('../models/user')
+
+userRouter.get('/', async (request, response) => {
+    const result = await User.find({})
+    response.json(result)
+})
+
+userRouter.post('/', async (request, response) => {
+    const newUser = request.body
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(newUser.password, saltRounds)
+
+    const newUserObject = new User({
+        username: newUser.username,
+        name: newUser.name,
+        passwordHash
+    })
+
+    const savedUser = await newUserObject.save()
+    response.json(savedUser)
+})
+
+module.exports = userRouter
