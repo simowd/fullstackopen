d85327616d1c2313186757070724b6190d8623be
Martin Campuzano <martin.campuzano.flores@gmail.com>
2022-01-05 15:16:00 -0400

    Finished 5.7

--- part5/bloglist-react/src/components/Blog.js
+++ part5/bloglist-react/src/components/Blog.js
@@ -1,8 +1,46 @@
-import React from 'react'
-const Blog = ({blog, key}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState, useEffect } from 'react'
 
+const Blog = ({ blog, key }) => {
+  const [visible, setVisible] = useState(true)
+
+  const containerStyle = {
+    border: '1px',
+    boderColor: 'black',
+    borderStyle: 'solid',
+    padding: '0.5rem',
+    margin: '0.5rem',
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const showTitle = () => {
+    return (
+      <div style={containerStyle}>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}> view </button>
+      </div>
+    )
+  }
+
+  const showDetails = () => {
+    return(
+      <div style={containerStyle}>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}> hide </button>
+        <br />
+        {blog.url}
+        <br />
+        likes {blog.likes} <button>like</button>
+        <br />
+        {blog.user.name}
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {visible ? showTitle() : showDetails()}
+    </div>
+  )
+}
 export default Blog
