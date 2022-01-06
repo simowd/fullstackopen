624fcfeb608edb50fee6f9a5afd11b51ce885594
Martin Campuzano <martin.campuzano.flores@gmail.com>
2022-01-06 10:00:42 -0400

    Finished 5.14

--- part5/bloglist-react/src/tests/Blog.test.js
+++ part5/bloglist-react/src/tests/Blog.test.js
@@ -3,11 +3,12 @@ import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Blog from '../components/Blog'
 
-describe('<Blog />',() => {
+describe('<Blog />', () => {
   let component
   const data = {
     title: 'testTitle',
     author: 'eljojos',
+    user: { name: 'Sergio' },
     url: 'hola.com',
     likes: 50
   }
@@ -16,7 +17,7 @@ describe('<Blog />',() => {
 
   beforeEach(() => {
     component = render(
-      <Blog blog={data} blogs={[]} setBlogs={mockCallback}/>
+      <Blog blog={data} blogs={[]} setBlogs={mockCallback} />
     )
   })
 
@@ -24,5 +25,14 @@ describe('<Blog />',() => {
     const info = component.container.querySelector('.blogWrapped')
     expect(info).toHaveTextContent('testTitle')
     expect(info).toHaveTextContent('eljojos')
+  })
+
+  test('Blog details are shown when clicked', () => {
+    const button = component.container.querySelector('button')
+    fireEvent.click(button)
+
+    const data = component.container.querySelector('.blogUnwrapped')
+    expect(data).toHaveTextContent('hola.com')
+    expect(data).toHaveTextContent('50')
   })
 })
