eff191ba6780da2ecf09b6a4a68376b6bc9758a0
Martin Campuzano <martin.campuzano.flores@gmail.com>
2022-01-08 16:30:32 -0400

    Finished 6.14

--- part6/anecdotes-redux/src/components/AnecdoteForm.js
+++ part6/anecdotes-redux/src/components/AnecdoteForm.js
@@ -1,15 +1,18 @@
 import React from "react"
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const addNote = (event) => {
+  const addNote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     console.log(content);
     event.target.anecdote.value = ''
+    const data = await anecdoteService.createNewAnecdote(content)
+    console.log(data);
     dispatch(createAnecdote(content))
   }
 
