99b8c2d2cada5e433517d7da1fcba5d5fa00918e
Martin Campuzano <martin.campuzano.flores@gmail.com>
2022-02-05 19:27:12 -0400

    Finished 8.17 and 8.18

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,18 +1,43 @@
 
+import { useApolloClient } from '@apollo/client'
 import React, { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
+import LoginForm from './components/LoginForm'
 import NewBook from './components/NewBook'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [token, setToken] = useState(null)
+  const client = useApolloClient()
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  const loginView = () => {
+    return <>
+      <button onClick={() => setPage('login')}>login</button>
+    </>
+  }
+
+  const logoutView = () => {
+    return <>
+      <button onClick={() => setPage('add')}>add book</button>
+      <button onClick={() => logout()}>logout</button>
+    </>
+  }
 
   return (
     <div>
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+        {
+          !token ? loginView() : logoutView()
+        }
       </div>
 
       <Authors
@@ -25,6 +50,12 @@ const App = () => {
 
       <NewBook
         show={page === 'add'}
+      />
+
+      <LoginForm
+        setToken={setToken}
+        setPage={setPage}
+        show={page === 'login'}
       />
 
     </div>
