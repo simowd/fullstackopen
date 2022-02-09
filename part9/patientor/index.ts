f393372edb888b4b4e65fe981456fc63d73af4b8
Martin Campuzano <martin.campuzano.flores@gmail.com>
2022-02-09 15:26:55 -0400

    Finished 9.8

---
+++ part9/patientor/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/ping', (_req, res) => {
+    console.log('pinged here');
+    res.send('pong')
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
