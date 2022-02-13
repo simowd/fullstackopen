a5edb4c62ee9103f1503607ec221a063b19b1f56
Martin Campuzano <martin.campuzano.flores@gmail.com>
2022-02-13 18:05:22 -0400

    Finished 9.19

--- part9/patientor/types.ts
+++ part9/patientor/types.ts
@@ -14,9 +14,51 @@ export interface PatientEntry {
     entries: Entry[]
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<DiagnoseEntry['code']>;
 }
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+
+export interface Discharge {
+    date: string,
+    criteria: string
+}
+
+export interface SickLeave {
+    startDate: string,
+    endDate: string
+}
+
+interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: "Hospital";
+    discharge: Discharge;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: "OccupationalHealthcare";
+    sickLeave: SickLeave;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export type Entry =
+    | HospitalEntry
+    | OccupationalHealthcareEntry
+    | HealthCheckEntry;
 
 export type NonSensitivePatientEntry = Omit<PatientEntry, 'ssn' | 'entries'>;
 export type NewPatient = Omit<PatientEntry, 'id'>;
