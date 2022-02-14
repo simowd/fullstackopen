da0bfbeb66dcdb48cacad939a6d36ab42fdf0a31
Martin Campuzano <martin.campuzano.flores@gmail.com>
2022-02-14 18:06:40 -0400

    Finished 9.22

--- part9/patientor-frontend/src/PatientComponent/EntriesContainer.tsx
+++ part9/patientor-frontend/src/PatientComponent/EntriesContainer.tsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 import React from "react";
 import { Diagnosis, Entry } from "../types";
-import EntryCard from "./EntryCard";
 import { apiBaseUrl } from "../constants";
+import HospitalEntry from "./HospitalEntry";
+import OccupationalHealthcareEntry from "./OccupationalHealthcareEntry";
+import HealthCheckEntry from "./HealthCheckEntry";
 
-const EntriesContainer = ({ entries } : {entries: Array<Entry> | undefined}): JSX.Element => {
+const EntriesContainer = ({ entries }: { entries: Array<Entry> | undefined }): JSX.Element => {
     const [diagnoses, setDiagnoses] = React.useState<Array<Diagnosis>>();
     React.useEffect(() => {
         if (!diagnoses) {
@@ -25,14 +27,37 @@ const EntriesContainer = ({ entries } : {entries: Array<Entry> | undefined}): JS
         }
     });
 
-    if(!entries){
+    if (!entries) {
         return <></>;
     }
 
-    return(
+    const assertNever = (value: never): never => {
+        throw new Error(
+            `Unhandled discriminated union member: ${JSON.stringify(value)}`
+        );
+    };
+
+    const EntryDetails = (entry: Entry) => {
+        switch (entry.type) {
+            case 'Hospital':
+                return <HospitalEntry entry={entry} diagnoses={diagnoses}/>;
+                break;
+            case 'OccupationalHealthcare':
+                return <OccupationalHealthcareEntry entry={entry} diagnoses={diagnoses}/>;
+                break;
+            case 'HealthCheck':
+                return <HealthCheckEntry entry={entry} diagnoses={diagnoses}/>;
+                break;
+            default:
+                return assertNever(entry);
+                break;
+        }
+    };
+
+    return (
         <div>
             <h3>entries</h3>
-            {entries.map((entry, index) => <EntryCard key={index} entry={entry} diagnoses={diagnoses}/>)}
+            {entries.map((entry: Entry) => EntryDetails(entry))}
         </div>
     );
 };
---
+++ part9/patientor-frontend/src/PatientComponent/HospitalEntry.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Icon } from "semantic-ui-react";
+import { Diagnosis, Entry } from "../types";
+
+interface MyProps {
+    entry: Entry;
+    diagnoses: Array<Diagnosis> | undefined;
+}
+
+const HospitalEntry = ({entry, diagnoses}: MyProps): JSX.Element => {
+    const style = {
+        border: "1px solid gray",
+        padding: "20px"
+    };
+
+    console.log(diagnoses);
+
+    return (
+        <div style={style}>
+            <h4>{entry.date} <Icon name='hospital'/></h4>
+            <p>{entry.description}</p>
+            {/* <ul>
+                {entry.diagnosisCodes?.map((code, index) => <li key={index}>{code} {diagnoses ? diagnoses.find((diag) => diag.code === code)?.name : ''}</li>)}
+            </ul> */}
+        </div>
+    );
+};
+
+export default HospitalEntry;
